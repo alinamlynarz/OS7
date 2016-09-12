b72bd774c0ed8ec1280939e96390a094b09eb230
H8 <reczek.krzysztof@gmail.com>
2016-09-12 22:21:47 +0200

    Documentation: Callback description

--- os7-core/src/main/java/com/h8/os7/core/common/Callback.java
+++ os7-core/src/main/java/com/h8/os7/core/common/Callback.java
@@ -1,5 +1,15 @@
 package com.h8.os7.core.common;
 
+/**
+ * Generic functional interface for callback methods.
+ * @param <T>
+ *     Generic type of a value returned.
+ */
 public interface Callback<T> {
+    /**
+     * Method is called when callback event occurs.
+     * @param value
+     *      Value returned at callback event.
+     */
     void call(T value);
 }
